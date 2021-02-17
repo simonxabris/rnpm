8dd148e675e9bf7ce8ffc765e4af939e22c36214
Ábris SIMON <abris96@gmail.com>
2021-02-17 21:58:20 +0100

    feat: add filter argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use dialoguer;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::env::args;
 use std::env::current_dir;
 use std::fs::read_to_string;
 use std::process::{Command, Stdio};
 
 fn main() {
+    let args: Vec<String> = args().collect();
+    let possible_search_arg = args.get(1);
+
     let mut cwd = current_dir().expect("Couldn't access current directory");
     cwd.push("package.json");
 
@@ -14,7 +18,19 @@ fn main() {
     let package_json: PackageJson =
         serde_json::from_str(&package_json_contents[..]).expect("package.json is not valid JSON.");
 
-    let scripts: Vec<&String> = package_json.scripts.keys().collect();
+    let scripts: Vec<&String> = match possible_search_arg {
+        Some(arg) => package_json
+            .scripts
+            .keys()
+            .filter(|script| script.contains(arg))
+            .collect(),
+        None => package_json.scripts.keys().collect(),
+    };
+
+    if scripts.len() == 0 {
+        println!("Could not find any scripts.");
+        std::process::exit(0);
+    }
 
     let selector = dialoguer::Select::with_theme(&dialoguer::theme::ColorfulTheme::default())
         .items(&scripts)
@@ -22,22 +38,16 @@ fn main() {
         .interact_on_opt(&dialoguer::console::Term::stdout())
         .expect("Failed to display options");
 
-    let selection = match selector {
-        Some(index) => index,
-        None => {
-            println!("None");
-            return;
-        }
-    };
+    let selected_script_index = selector.expect("No value was selected.");
 
     let mut child = Command::new("npm")
         .arg("run")
-        .arg(scripts[selection])
+        .arg(scripts[selected_script_index])
         .stdout(Stdio::inherit())
         .spawn()
-        .expect("err");
+        .expect("Failed to spawn child process.");
 
-    child.wait().expect("err");
+    child.wait().expect("Child Process failed");
 }
 
 #[derive(Serialize, Deserialize, Debug)]
