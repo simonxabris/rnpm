8fea13a162a3ee0c64ebee5df34c1358ceb941aa
Ábris Simon <abris96@gmail.com>
2021-04-21 19:24:42 +0200

    feat: add flag that runs script if there is an exact match to the search arg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{App, Arg};
 use dialoguer;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, process};
 use std::env::current_dir;
 use std::fs::read_to_string;
 use std::process::{Command, Stdio};
@@ -9,7 +9,11 @@ use std::process::{Command, Stdio};
 fn main() {
     let args = App::new("rnpm")
         .about("Find the script you're looking for.")
-        .arg(Arg::with_name("script").takes_value(false).required(false))
+        .arg(Arg::with_name("script")
+            .takes_value(false)
+            .required(false)
+            .help("The string you're searching for")
+        )
         .arg(
             Arg::with_name("manager")
                 .short("m")
@@ -19,13 +23,21 @@ fn main() {
                 .possible_values(&["npm", "yarn", "pnpm"])
                 .required(false)
         )
+        .arg(
+            Arg::with_name("run-exact")
+                .short("r")
+                .long("run-exact")
+                .takes_value(false)
+                .required(false)
+                .help("If set, immediately run the script that matches exactly.")
+        )
         .get_matches();
 
     let has_yarn_lock = std::fs::File::open("yarn.lock").is_ok();
     let has_pnpm_lock = std::fs::File::open("pnpm-lock.yaml").is_ok();
 
     let possible_search_arg = args.value_of("script");
-    let possible_package_manager = if let Some(manager) = args.value_of("manager") {
+    let package_manager = if let Some(manager) = args.value_of("manager") {
         manager
     } else {
         match (has_yarn_lock, has_pnpm_lock) {
@@ -34,6 +46,9 @@ fn main() {
             _ => "npm"
         }
     };
+    let run_exact_flag = args.is_present("run-exact");
+
+    println!("run exact flag: {:?}", run_exact_flag);
 
     let mut cwd = current_dir().expect("Couldn't access current directory");
     cwd.push("package.json");
@@ -43,13 +58,19 @@ fn main() {
     let package_json: PackageJson =
         serde_json::from_str(&package_json_contents[..]).expect("package.json is not valid JSON.");
 
-    let scripts: Vec<&String> = match possible_search_arg {
-        Some(arg) => package_json
-            .scripts
-            .keys()
-            .filter(|script| script.contains(arg))
-            .collect(),
-        None => package_json.scripts.keys().collect(),
+    let scripts: Vec<&String> = if let Some(arg) = possible_search_arg {
+        let mut matching_scripts: Vec<&String> = Vec::new();
+        for script in package_json.scripts.keys() {
+            if script == arg && run_exact_flag {
+                execute_command(package_manager, script);
+            } else if script.contains(arg) {
+                matching_scripts.push(script);
+            }
+        }
+
+        matching_scripts
+    } else {
+        package_json.scripts.keys().collect()
     };
 
     if scripts.len() == 0 {
@@ -65,14 +86,20 @@ fn main() {
 
     let selected_script_index = selector.expect("No value was selected.");
 
-    let mut child = Command::new(possible_package_manager)
+    execute_command(package_manager, scripts[selected_script_index]);
+}
+
+fn execute_command(package_manager: &str, command: &str) -> ! {
+    let mut child = Command::new(package_manager)
         .arg("run")
-        .arg(scripts[selected_script_index])
+        .arg(command)
         .stdout(Stdio::inherit())
         .spawn()
         .expect("Failed to spawn child process.");
 
     child.wait().expect("Child Process failed");
+
+    process::exit(0);
 }
 
 #[derive(Serialize, Deserialize, Debug)]
