23cf3f65fca7d85265f48e74d0c8d9955ee84d0b
Simon Ábris <abris.simon@goto.com>
2023-10-06 20:23:21 +0200

    add monorepo and bun support

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ mod select;
 use clap::{App, Arg};
 use serde::{Deserialize, Serialize};
 use std::env::current_dir;
-use std::fs::read_to_string;
+use std::ffi::OsStr;
+use std::fs::{read_to_string, File};
+use std::path::Path;
 use std::process::{Command, Stdio};
+use std::str::FromStr;
 use std::{collections::HashMap, process};
 
 use select::Select;
@@ -37,25 +40,17 @@ fn main() {
         )
         .get_matches();
 
-    let has_yarn_lock = std::fs::File::open("yarn.lock").is_ok();
-    let has_pnpm_lock = std::fs::File::open("pnpm-lock.yaml").is_ok();
+    let cwd = current_dir().expect("Couldn't access current directory");
+    let mut package_json_path = cwd.clone();
+    package_json_path.push("package.json");
 
     let possible_search_arg = args.value_of("script");
-    let package_manager = if let Some(manager) = args.value_of("manager") {
-        manager
-    } else {
-        match (has_yarn_lock, has_pnpm_lock) {
-            (true, false) => "yarn",
-            (false, true) => "pnpm",
-            _ => "npm",
-        }
-    };
+    let package_manager =
+        find_lockfile_up_tree(cwd).expect("could not find a valid lockfile up the tre");
     let run_exact_flag = args.is_present("run-exact");
 
-    let mut cwd = current_dir().expect("Couldn't access current directory");
-    cwd.push("package.json");
-
-    let package_json_contents = read_to_string(cwd).expect("Could not find package.json file.");
+    let package_json_contents =
+        read_to_string(package_json_path).expect("Could not find package.json file.");
 
     let package_json: PackageJson =
         serde_json::from_str(&package_json_contents[..]).expect("package.json is not valid JSON.");
@@ -66,7 +61,7 @@ fn main() {
             .keys()
             .filter(|script| {
                 if script == &arg && run_exact_flag {
-                    execute_command(package_manager, arg);
+                    execute_command(&package_manager, arg);
                 }
 
                 script.contains(arg)
@@ -81,7 +76,7 @@ fn main() {
     } else if scripts.len() == 1 {
         println!("Running script: {}", scripts[0]);
 
-        execute_command(package_manager, scripts[0]);
+        execute_command(&package_manager, scripts[0]);
     }
 
     scripts.sort_by(|a, b| a.cmp(b));
@@ -94,10 +89,10 @@ fn main() {
     println!("Running script: {}", selected_script);
     println!("\r");
 
-    execute_command(package_manager, selected_script);
+    execute_command(&package_manager, selected_script);
 }
 
-fn execute_command(package_manager: &str, command: &str) -> ! {
+fn execute_command(package_manager: &PackageManager, command: &str) -> ! {
     let mut child = Command::new(package_manager)
         .arg("run")
         .arg(command)
@@ -108,6 +103,67 @@ fn execute_command(package_manager: &str, command: &str) -> ! {
     child.wait().expect("Child Process failed");
 
     process::exit(0);
+}
+
+#[derive(Debug, PartialEq)]
+enum PackageManager {
+    Npm(String),
+    Pnpm(String),
+    Yarn(String),
+    Bun(String),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseManagerError;
+
+impl FromStr for PackageManager {
+    type Err = ParseManagerError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "npm" => Ok(Self::Npm(String::from("npm"))),
+            "pnpm" => Ok(Self::Pnpm(String::from("pnpm"))),
+            "yarn" => Ok(Self::Yarn(String::from("yarn"))),
+            "bun" => Ok(Self::Bun(String::from("bun"))),
+            _ => Err(ParseManagerError),
+        }
+    }
+}
+
+impl AsRef<OsStr> for PackageManager {
+    fn as_ref(&self) -> &OsStr {
+        match self {
+            PackageManager::Npm(v) => OsStr::new(v),
+            PackageManager::Pnpm(v) => OsStr::new(v),
+            PackageManager::Yarn(v) => OsStr::new(v),
+            PackageManager::Bun(v) => OsStr::new(v),
+        }
+    }
+}
+
+fn find_lockfile_up_tree<P: AsRef<Path>>(path_like: P) -> Option<PackageManager> {
+    let managers = [
+        ("npm", "package-lock.json"),
+        ("yarn", "yarn.lock"),
+        ("pnpm", "pnpm-lock.yaml"),
+        ("bun", "bun.lockb"),
+    ];
+    let path = path_like.as_ref().to_path_buf();
+
+    for (manager, lockfile) in managers.iter() {
+        let mut lockfile_path = path.clone();
+        lockfile_path.push(lockfile);
+        let has_lockfile = File::open(lockfile_path).is_ok();
+
+        if has_lockfile {
+            return PackageManager::from_str(manager).ok();
+        }
+    }
+
+    match path.parent() {
+        Some(p) => find_lockfile_up_tree(p),
+        None => None,
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
